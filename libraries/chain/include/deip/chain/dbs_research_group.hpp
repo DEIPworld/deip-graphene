01025943b8ac44095520c23afc5ebfcf5ab7216e
Alexey Kulik <kulik.aliaksei@gmail.com>
2018-01-20 14:38:51 +0300

    Consolidate dbs_research_group and dbs_research_group_token

--- libraries/chain/include/deip/chain/dbs_research_group.hpp
+++ libraries/chain/include/deip/chain/dbs_research_group.hpp
@@ -26,7 +26,7 @@ public:
 
     /** Get research group by id
      */
-    const research_group_object& get_research_group(research_group_id_type id) const;
+    const research_group_object& get_research_group(const research_group_id_type& id) const;
 
     /** Create research_group object.
      *
@@ -34,44 +34,27 @@ public:
      */
     const research_group_object& create_research_group(const string permlink, const string description);
 
-    void change_quorum_group_object(u_int16_t quorum_percent, research_group_id_type research_group_id);
+    void change_quorum(const uint32_t quorum_percent, const research_group_id_type& research_group_id);
 
-    void check_research_group_existence(research_group_id_type research_group_id) const;
+    void check_research_group_existence(const research_group_id_type& research_group_id) const;
 
-};
+    const research_group_token_object& get_research_group_token_by_id(const research_group_token_id_type& id) const;
 
-class dbs_research_group_token : public dbs_base
-{
-    friend class dbservice_dbs_factory;
+    const research_group_token_object& get_research_group_token_by_account(const account_name_type& account,
+                                                                const research_group_id_type& research_group_id) const;
 
-    dbs_research_group_token() = delete;
-protected:
-    explicit dbs_research_group_token(database& db);
-
-public:
-
-    /** Get research group token by id
-     */
-    const research_group_token_object& get_research_group_token(research_group_token_id_type id) const;
-
-    const research_group_token_object& get_research_group_token(const account_name_type account_name, 
-                                                                const research_group_id_type research_group_id) const;
-    /** Create research_group_token object.
-     *
-     * @returns research group token object
-     */
-
-    const research_group_token_object& create_research_group_token(const research_group_id_type research_group,
+    const research_group_token_object& create_research_group_token(const research_group_id_type& research_group_id,
                                                                    const share_type amount,
-                                                                   const account_name_type account_name);
+                                                                   const account_name_type& account);
 
-    void remove_token_object(const account_name_type& account_name, research_group_id_type research_group_id);
+    void remove_token(const account_name_type& account, const research_group_id_type& research_group_id);
 
-    void check_research_token_existence(const account_name_type& account_name,
-                      research_group_id_type research_group_id) const;
+    void check_research_token_existence(const account_name_type& account,
+                                        const research_group_id_type& research_group_id) const;
 
-    void add_share_to_research_group_token(const share_type amount, 
-                                           const research_group_token_object& research_group_token);
+    void update_research_group_token_share(const share_type amount,
+                                           const research_group_token_id_type& token_id);
+
 };
 
 } // namespace chain
