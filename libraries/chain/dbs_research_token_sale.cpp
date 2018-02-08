d68a0b6f7ddd459edd3f3ba9443f8c5bb49c50d7
Dzeranov <sergey.dzeranov@gmail.com>
2018-02-08 10:34:50 +0300

    fix build

--- libraries/chain/dbs_research_token_sale.cpp
+++ libraries/chain/dbs_research_token_sale.cpp
@@ -9,19 +9,19 @@ dbs_research_token_sale::dbs_research_token_sale(database& db)
 {
 }
 
-const research_token_sale_object&
-dbs_research_token_sale::start_research_token_sale(const research_id_type& research_id,
-                                                   const fc::time_point start_time,
-                                                   const fc::time_point end_time,
-                                                   const deip::chain::share_type balance_tokens,
-                                                   const deip::chain::share_type soft_cap,
-                                                   const deip::chain::share_type hard_cap)
+const research_token_sale_object& dbs_research_token_sale::start_research_token_sale(const research_id_type& research_id,
+                                                                                     const fc::time_point start_time,
+                                                                                     const fc::time_point end_time,
+                                                                                     const deip::chain::share_type balance_tokens,
+                                                                                     const deip::chain::share_type soft_cap,
+                                                                                     const deip::chain::share_type hard_cap)
 {
     const research_token_sale_object& new_research_token_sale
         = db_impl().create<research_token_sale_object>([&](research_token_sale_object& research_token_sale) {
               research_token_sale.research_id = research_id;
               research_token_sale.start_time = start_time;
               research_token_sale.end_time = end_time;
+              research_token_sale.total_amount = 0;
               research_token_sale.balance_tokens = balance_tokens;
               research_token_sale.soft_cap = soft_cap;
               research_token_sale.hard_cap = hard_cap;
@@ -32,3 +32,4 @@ dbs_research_token_sale::start_research_token_sale(const research_id_type& resea
 
 } // namespace chain
 } // namespace deip
+
