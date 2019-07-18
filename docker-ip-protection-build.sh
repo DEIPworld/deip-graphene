b46829bb461f376779b1022e35fa793ae06e30e7
egortsaryk9 <egortsaryk@gmail.com>
2019-07-18 06:03:56 +0300

    PROD build configuration

--- docker-ip-protection-build.sh
+++ docker-ip-protection-build.sh
@@ -7,7 +7,7 @@ LATEST="latest"
 echo "Building deipworld/ip-protection-platform-full-node image..."
 export IMAGE_NAME="deipworld/ip-protection-platform-full-node:$TAG"
 export LATEST_IMAGE_NAME="deipworld/ip-protection-platform-full-node:$LATEST"
-docker build -t=${IMAGE_NAME} -f docker/Dockerfile.fullnode --build-arg BUILD_TESTNET=ON .
+docker build -t=${IMAGE_NAME} -f docker/Dockerfile.fullnode --build-arg BUILD_DEIP_TEST_NET=OFF LOW_MEMORY_NODE=OFF MAKE_BUILD_TYPE=Release .
 docker tag ${IMAGE_NAME} ${LATEST_IMAGE_NAME}
 docker push ${IMAGE_NAME}
 docker push ${LATEST_IMAGE_NAME}
