c3f32a358a261f968a9a1a9a8518507034c9c057
egortsaryk9 <egortsaryk@gmail.com>
2019-08-06 14:44:04 +0300

    Local node configuration

--- docker-ip-protection-build.sh
+++ docker-ip-protection-build.sh
@@ -11,3 +11,11 @@ docker build -t=${IMAGE_NAME} -f docker/Dockerfile.fullnode --build-arg BUILD_DE
 docker tag ${IMAGE_NAME} ${LATEST_IMAGE_NAME}
 docker push ${IMAGE_NAME}
 docker push ${LATEST_IMAGE_NAME}
+
+echo "Building deipworld/ip-protection-platform-local-node image..."
+export IMAGE_NAME="deipworld/ip-protection-platform-local-node:$TAG"
+export LATEST_IMAGE_NAME="deipworld/ip-protection-platform-local-node:$LATEST"
+docker build -t=${IMAGE_NAME} -f docker/Dockerfile.local --build-arg BUILD_DEIP_TEST_NET=OFF --build-arg LOW_MEMORY_NODE=OFF --build-arg MAKE_BUILD_TYPE=Release .
+docker tag ${IMAGE_NAME} ${LATEST_IMAGE_NAME}
+docker push ${IMAGE_NAME}
+docker push ${LATEST_IMAGE_NAME}
