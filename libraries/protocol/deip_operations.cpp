df414e3cce473c628deada43f088f8e09d1d1f85
egortsaryk9 <egortsaryk@gmail.com>
2019-09-21 15:29:27 +0300

    Assert different accounts in contracts

--- libraries/protocol/deip_operations.cpp
+++ libraries/protocol/deip_operations.cpp
@@ -327,7 +327,8 @@ void create_nda_contract_operation::validate() const
     validate_account_name(party_a);
     validate_account_name(party_b);
     validate_account_name(contract_creator);
-    
+    FC_ASSERT(party_a != party_b, "Parties must not be the same accounts");
+
     // WARNING: Currently we are not supporting sharing files by both sides within a single NDA contract
     FC_ASSERT(contract_creator == party_a, "Two-way NDA contracts are not supported currently");
     FC_ASSERT(disclosing_party.size() == 1, "Two-way NDA contracts are not supported currently");
