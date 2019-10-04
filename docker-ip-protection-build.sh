37600a6bb2d216c54c61d5bfc3ebe2fb14bbfc7a
egortsaryk9 <egortsaryk@gmail.com>
2019-10-04 19:30:26 +0300

    Dev-ops files

--- docker-ip-protection-build.sh
+++ docker-ip-protection-build.sh
@@ -16,23 +16,23 @@ echo "Building deipworld/ip-protection-platform-full-node $STAGING_TAG image..."
 export IMAGE_NAME="deipworld/ip-protection-platform-full-node:${STAGING_TAG}${TAG}"
 export LATEST_IMAGE_NAME="deipworld/ip-protection-platform-full-node:${STAGING_TAG}${LATEST}"
 docker build -t=${IMAGE_NAME} -f docker/Dockerfile.fullnode --build-arg BUILD_DEIP_TEST_NET=OFF --build-arg LOW_MEMORY_NODE=OFF --build-arg MAKE_BUILD_TYPE=Release .
-# docker tag ${IMAGE_NAME} ${LATEST_IMAGE_NAME}
-# docker push ${IMAGE_NAME}
-# docker push ${LATEST_IMAGE_NAME}
-# docker rmi ${IMAGE_NAME}
-# docker rmi ${LATEST_IMAGE_NAME}
+docker tag ${IMAGE_NAME} ${LATEST_IMAGE_NAME}
+docker push ${IMAGE_NAME}
+docker push ${LATEST_IMAGE_NAME}
+docker rmi ${IMAGE_NAME}
+docker rmi ${LATEST_IMAGE_NAME}
 
 
-# # Build local image for prod only
-# if [ "$STAGING_TAG" == "" ]
-# then
-#     echo "Building deipworld/ip-protection-platform-local-node image..."
-#     export IMAGE_NAME="deipworld/ip-protection-platform-local-node:$TAG"
-#     export LATEST_IMAGE_NAME="deipworld/ip-protection-platform-local-node:$LATEST"
-#     docker build -t=${IMAGE_NAME} -f docker/Dockerfile.local --build-arg BUILD_DEIP_TEST_NET=OFF --build-arg LOW_MEMORY_NODE=OFF --build-arg MAKE_BUILD_TYPE=Release .
-#     docker tag ${IMAGE_NAME} ${LATEST_IMAGE_NAME}
-#     docker push ${IMAGE_NAME}
-#     docker push ${LATEST_IMAGE_NAME}
-#     docker rmi ${IMAGE_NAME}
-#     docker rmi ${LATEST_IMAGE_NAME}
-# fi
+# Build local image for prod only
+if [ "$STAGING_TAG" == "" ]
+then
+    echo "Building deipworld/ip-protection-platform-local-node image..."
+    export IMAGE_NAME="deipworld/ip-protection-platform-local-node:$TAG"
+    export LATEST_IMAGE_NAME="deipworld/ip-protection-platform-local-node:$LATEST"
+    docker build -t=${IMAGE_NAME} -f docker/Dockerfile.local --build-arg BUILD_DEIP_TEST_NET=OFF --build-arg LOW_MEMORY_NODE=OFF --build-arg MAKE_BUILD_TYPE=Release .
+    docker tag ${IMAGE_NAME} ${LATEST_IMAGE_NAME}
+    docker push ${IMAGE_NAME}
+    docker push ${LATEST_IMAGE_NAME}
+    docker rmi ${IMAGE_NAME}
+    docker rmi ${LATEST_IMAGE_NAME}
+fi
