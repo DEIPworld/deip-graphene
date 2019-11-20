6c29c6de67d2a2e7acbc4ae9b221da90f20e6aa4
Sergey <sergey.dzeranov@gmail.com>
2019-11-20 16:56:16 +0300

    block apis moved to plugin

--- libraries/plugins/blockchain_history/include/deip/blockchain_history/blockchain_history_api.hpp
+++ libraries/plugins/blockchain_history/include/deip/blockchain_history/blockchain_history_api.hpp
@@ -1,29 +1,42 @@
 #pragma once
 #include <fc/api.hpp>
+#include <deip/app/deip_api_objects.hpp>
 #include <deip/blockchain_history/applied_operation.hpp>
 #include <deip/protocol/transaction.hpp>
 #include <map>
+
+
 namespace deip {
 namespace app {
+
 struct api_context;
 class application;
+
 } // namespace app
 } // namespace deip
+
 namespace deip {
 namespace blockchain_history {
+
 using deip::protocol::annotated_signed_transaction;
 using deip::protocol::transaction_id_type;
+
 namespace detail {
 class blockchain_history_api_impl;
 }
+
 class blockchain_history_api
 {
 public:
     blockchain_history_api(const deip::app::api_context& ctx);
+
     ~blockchain_history_api();
+
     void on_api_startup();
+
     std::map<uint32_t, applied_operation>
     get_ops_history(uint32_t from_op, uint32_t limit, const applied_operation_type& opt) const;
+
     /**
      *  @brief Get sequence of operations included/generated within a particular block
      *  @param block_num Height of the block whose generated virtual operations should be returned
@@ -31,11 +44,27 @@ public:
      *  @return sequence of operations included/generated within the block
      */
     std::map<uint32_t, applied_operation> get_ops_in_block(uint32_t block_num, applied_operation_type opt) const;
+
     annotated_signed_transaction get_transaction(transaction_id_type trx_id) const;
+
+    optional<protocol::block_header> get_block_header(uint32_t block_num) const;
+
+    std::map<uint32_t, protocol::block_header> get_block_headers_history(uint32_t block_num, uint32_t limit) const;
+
+    std::map<uint32_t, app::signed_block_api_obj> get_blocks_history(uint32_t block_num, uint32_t limit) const;
 
 private:
     std::unique_ptr<detail::blockchain_history_api_impl> _impl;
 };
+
 } // namespace blockchain_history
 } // namespace deip
-FC_API(deip::blockchain_history::blockchain_history_api, (get_ops_history)(get_ops_in_block)(get_transaction))
+
+FC_API(deip::blockchain_history::blockchain_history_api,
+        (get_ops_history)
+        (get_ops_in_block)
+        (get_transaction)
+        (get_block_header)
+        (get_block_headers_history)
+        (get_blocks_history)
+)
--- libraries/wallet/wallet.cpp
+++ libraries/wallet/wallet.cpp
@@ -1006,7 +1006,8 @@ bool wallet_api::copy_wallet_file(const string& destination_filename)
 
 optional<block_header> wallet_api::get_block_header(uint32_t num) const
 {
-    return my->_remote_db->get_block_header(num);
+    my->use_remote_blockchain_history_api();
+    return (*my->_remote_blockchain_history_api)->get_block_header(num);
 }
 
 optional<signed_block_api_obj> wallet_api::get_block(uint32_t num) const
@@ -1016,12 +1017,14 @@ optional<signed_block_api_obj> wallet_api::get_block(uint32_t num) const
 
 std::map<uint32_t, block_header> wallet_api::get_block_headers_history(uint32_t num, uint32_t limit) const
 {
-    return my->_remote_db->get_block_headers_history(num, limit);
+    my->use_remote_blockchain_history_api();
+    return (*my->_remote_blockchain_history_api)->get_block_headers_history(num, limit);
 }
 
 std::map<uint32_t, signed_block_api_obj> wallet_api::get_blocks_history(uint32_t num, uint32_t limit) const
 {
-    return my->_remote_db->get_blocks_history(num, limit);
+    my->use_remote_blockchain_history_api();
+    return (*my->_remote_blockchain_history_api)->get_blocks_history(num, limit);
 }
 
 std::map<uint32_t, applied_operation> wallet_api::get_ops_in_block(uint32_t block_num, applied_operation_type opt) const
