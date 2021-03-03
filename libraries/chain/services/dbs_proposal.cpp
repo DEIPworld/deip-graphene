c91fd0533e76f5625647c12b113c01d0f6c135d3
egortsaryk9 <egortsaryk@gmail.com>
2021-03-03 07:12:03 +0100

    Assertion removed

--- libraries/chain/services/dbs_proposal.cpp
+++ libraries/chain/services/dbs_proposal.cpp
@@ -94,8 +94,8 @@ const proposal_object& dbs_proposal::create_proposal(
     auto& dgp_service = db_impl().obtain_service<dbs_dynamic_global_properties>();
     const auto block_time = db_impl().head_block_time();
 
-    FC_ASSERT(proposed_trx.expiration == expiration_time,
-      "Proposal expiration time must be equal to proposed transaction expiration time");
+    // FC_ASSERT(proposed_trx.expiration == expiration_time,
+    //   "Proposal expiration time must be equal to proposed transaction expiration time");
 
     std::vector<account_name_type> owner_active_diff;
 
