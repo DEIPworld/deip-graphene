dda5daaf1a78c9f73124ec2cda438c35ad7fb50a
egortsaryk9 <egortsaryk@gmail.com>
2021-09-24 14:16:35 +0200

    Inheritance for accept_contract_agreement op fixed

--- libraries/protocol/include/deip/protocol/operations/accept_contract_agreement.hpp
+++ libraries/protocol/include/deip/protocol/operations/accept_contract_agreement.hpp
@@ -8,15 +8,12 @@
 namespace deip {
 namespace protocol {
 
-struct accept_contract_agreement_operation: public entity_operation
+struct accept_contract_agreement_operation : public base_operation
 {
     external_id_type external_id;
     account_name_type party;
 
     extensions_type extensions;
-
-    string entity_id() const { return "external_id"; }
-    external_id_type get_entity_id() const { return external_id; }
 
     void validate() const;
 
